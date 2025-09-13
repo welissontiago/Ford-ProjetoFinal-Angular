de40833d36c8f56f6e034798839022d023a04b94
Uelisson <tagots09@gmail.com>
2025-09-13 17:01:19 -0300

    Adicionado video de background

---
+++ src/app/components/home/video/video.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-video',
+  imports: [],
+  templateUrl: './video.component.html',
+  styleUrl: './video.component.css'
+})
+export class VideoComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { VideoComponent } from '../../components/home/video/video.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, VideoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

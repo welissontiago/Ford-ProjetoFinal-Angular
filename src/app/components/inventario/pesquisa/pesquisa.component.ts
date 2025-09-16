dec40146a7a7fd995d3976914486628bbe7e4c18
Uelisson <tagots09@gmail.com>
2025-09-16 05:33:16 -0300

    Começando tela de veiculos

--- src/app/components/inventario/pesquisa/pesquisa.component.ts
+++ src/app/components/inventario/pesquisa/pesquisa.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
 
 @Component({
   selector: 'app-pesquisa',
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatIconModule,
+  ],
   templateUrl: './pesquisa.component.html',
-  styleUrl: './pesquisa.component.css'
+  styleUrl: './pesquisa.component.css',
 })
 export class PesquisaComponent {
-
+  emailFormControl = new FormControl('', []);
+  matcher = new MyErrorStateMatcher();
 }
--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { TitleComponent } from './title/title.component';
+import { PesquisaComponent } from '../../components/inventario/pesquisa/pesquisa.component';
 
 @Component({
   selector: 'app-inventario',
-  imports: [HeaderComponent, FooterComponent, TitleComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    TitleComponent,
+    PesquisaComponent,
+  ],
   templateUrl: './inventario.component.html',
   styleUrl: './inventario.component.css',
 })

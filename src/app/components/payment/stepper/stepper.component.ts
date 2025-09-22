814756fc547d27320f13827f7ad204516122146c
UELISSON TIAGO SILVA SANTOS <020.732974@universidadesenaicimatec.edu.br>
2025-09-22 16:47:49 -0300

    Completo primeiro step

--- src/app/components/payment/stepper/stepper.component.ts
+++ src/app/components/payment/stepper/stepper.component.ts
@@ -1,11 +1,25 @@
-import { Component, EventEmitter, inject, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatStepperModule } from '@angular/material/stepper';
 import { Cores } from '../../../core/models/cores.model';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stepper',
@@ -17,12 +31,12 @@ import { CommonModule } from '@angular/common';
     MatInputModule,
     MatButtonModule,
     CommonModule,
+    MatIconModule,
   ],
   templateUrl: './stepper.component.html',
-  styleUrl: './stepper.component.css'
+  styleUrl: './stepper.component.css',
 })
-export class StepperComponent implements OnChanges{
-
+export class StepperComponent implements OnChanges {
   @Input() cores: Cores[] = [];
   @Input() corSelecionada?: Cores;
   @Output() corSelecionadaChange = new EventEmitter<Cores>();
@@ -44,14 +58,13 @@ export class StepperComponent implements OnChanges{
   isEditable = true;
 
   selecionarCor(cor: Cores) {
-  this.corSelecionadaChange.emit(cor);
-  this.firstFormGroup.controls.corCtrl.setValue(cor);
-}
+    this.corSelecionadaChange.emit(cor);
+    this.firstFormGroup.controls.corCtrl.setValue(cor);
+  }
 
-ngOnChanges(changes: SimpleChanges) {
+  ngOnChanges(changes: SimpleChanges) {
     if (changes['corSelecionada'] && this.corSelecionada) {
       this.firstFormGroup.controls.corCtrl.setValue(this.corSelecionada);
     }
   }
-
 }

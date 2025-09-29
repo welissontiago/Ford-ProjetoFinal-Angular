034fab2fa4ff2a32c198dab5ab0083058ee7238a
UELISSON TIAGO SILVA SANTOS <200028937@ucsal.br>
2025-09-29 09:01:36 -0300

    Feito a tela de sucesso de pagamento

--- src/app/components/payment/modal/success-modal/success-modal.component.ts
+++ src/app/components/payment/modal/success-modal/success-modal.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+import { CdkStepLabel } from "@angular/cdk/stepper";
 
 @Component({
   selector: 'app-success-modal',
   templateUrl: './success-modal.component.html',
   styleUrls: ['./success-modal.component.css'],
+  imports: [CdkStepLabel],
 })
-export class SuccessModalComponent {
+export class SuccessModalComponent implements AfterViewInit {
+  @ViewChild('dotlottieCanvas') dotlottieCanvas!: ElementRef<HTMLCanvasElement>;
+
   constructor(public dialogRef: MatDialogRef<SuccessModalComponent>) {}
+
+  ngAfterViewInit(): void {
+    new DotLottie({
+      autoplay: true,
+      loop: true,
+      canvas: this.dotlottieCanvas.nativeElement,
+      src: 'https://lottie.host/41fdb70d-cc13-48de-8f99-0ef96bd252b9/S1Ca3vRmaT.lottie',
+    });
+  }
 
   close(): void {
     this.dialogRef.close();

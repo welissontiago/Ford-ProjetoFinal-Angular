375abc86f3b58145cc1851353bdb0a153a7146f0
UELISSON TIAGO SILVA SANTOS <200028937@ucsal.br>
2025-10-03 11:00:54 -0300

    Mudada a cor do botão do menu inicial para ficar correto

--- src/app/components/home/categorias/categorias.component.ts
+++ src/app/components/home/categorias/categorias.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-categorias',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './categorias.component.html',
   styleUrl: './categorias.component.css',
 })

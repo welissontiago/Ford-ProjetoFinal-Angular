b95794b411dcaf1010efaa0232a5c0785ab24891
Uelisson <tagots09@gmail.com>
2025-10-05 13:58:30 -0300

    Completa a tela de veiculos

---
+++ src/app/components/admin/veiculos/vehicle-list/vehicle-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { Cars } from '../../../../core/models/cars.model';
+import { CarsService } from '../../../../core/services/cars.service';
+import { forkJoin, Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-vehicle-list',
+  imports: [
+    MatDividerModule,
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    RouterModule,
+  ],
+  templateUrl: './vehicle-list.component.html',
+  styleUrl: './vehicle-list.component.css',
+})
+export class VehicleListComponent implements OnInit {
+  @Input() car!: Cars;
+  cars: Cars[] = [];
+  featuredCarsCount = 0;
+  @Output() deleteRequested = new EventEmitter<number | string>();
+  public AllVehicles$!: Observable<Cars[]>;
+  public deletedCars$!: Observable<Cars>;
+
+  constructor(private carsService: CarsService) {}
+
+  ngOnInit(): void {
+    forkJoin({
+      cars: this.carsService.getCars(),
+      featuredCars: this.carsService.getFeaturedCars(),
+    }).subscribe(({ cars, featuredCars }) => {
+      this.cars = cars;
+      this.featuredCarsCount = featuredCars.length;
+    });
+  }
+
+  deleteCar(id: number | string): void {
+    this.carsService.deleteCar(id).subscribe({
+      next: () => {
+        console.log(`Carro ${id} deletado com sucesso!`);
+        this.AllVehicles$ = this.carsService.getCars();
+      },
+      error: (err) => {
+        console.error('Erro ao deletar carro:', err);
+      },
+    });
+  }
+
+  onDelete(): void {
+    this.deleteRequested.emit(this.car.id);
+  }
+}
--- src/app/components/admin/veiculos/veiculos.component.ts
+++ src/app/components/admin/veiculos/veiculos.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
 @Component({
   selector: 'app-veiculos',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    RouterModule,
+    MatButtonModule,
+    VehicleListComponent,
+  ],
   templateUrl: './veiculos.component.html',
-  styleUrl: './veiculos.component.css'
+  styleUrl: './veiculos.component.css',
 })
-export class VeiculosComponent {
-
-}
+export class VeiculosComponent {}

d0ccfd43ed9ca2f8bc2eb882ac95d1181cf7e855
UELISSON TIAGO SILVA SANTOS <020.732974@universidadesenaicimatec.edu.br>
2025-10-06 15:24:32 -0300

    Começando a fazer os filtros

--- src/app/components/admin/veiculos/vehicle-list/vehicle-list.component.ts
+++ src/app/components/admin/veiculos/vehicle-list/vehicle-list.component.ts
@@ -2,7 +2,13 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { Cars } from '../../../../core/models/cars.model';
 import { CarsService } from '../../../../core/services/cars.service';
-import { forkJoin, Observable } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  forkJoin,
+  map,
+  Observable,
+} from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -26,6 +32,14 @@ export class VehicleListComponent implements OnInit {
   featuredCarsCount = 0;
   public AllVehicles$!: Observable<Cars[]>;
   public deletedCars$!: Observable<Cars>;
+  public carsFiltrados$!: Observable<Cars[]>;
+
+  private filtrosSubject = new BehaviorSubject<any>({
+    categoria: null,
+    status: null,
+    combustivel: null,
+    nome: null,
+  });
 
   constructor(private carsService: CarsService) {}
 
@@ -37,6 +51,31 @@ export class VehicleListComponent implements OnInit {
       this.cars = cars;
       this.featuredCarsCount = featuredCars.length;
     });
+
+    const allCars$ = this.carsService.getCars();
+
+    this.carsFiltrados$ = combineLatest([allCars$, this.filtrosSubject]).pipe(
+      map(([cars, filtros]) => {
+        return cars.filter((car) => {
+          const atendeCategoria =
+            !filtros.categoria ||
+            car.categoria.toLowerCase() === filtros.categoria.toLowerCase();
+
+          const status = !filtros.status || car.estoque === +filtros.estoque;
+
+          const atendeCombustivel =
+            !filtros.combustivel ||
+            car.combustiveis
+              .toLowerCase()
+              .includes(filtros.combustivel.toLowerCase());
+          return atendeCategoria && status && atendeCombustivel;
+        });
+      })
+    );
+  }
+
+  aplicarFiltros(filtros: any) {
+    this.filtrosSubject.next(filtros);
   }
 
   deleteCar(id: number | string): void {
--- src/app/components/admin/veiculos/veiculos.component.ts
+++ src/app/components/admin/veiculos/veiculos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,6 +6,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from '@angular/router';
 import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-veiculos',
@@ -17,8 +19,26 @@ import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
     RouterModule,
     MatButtonModule,
     VehicleListComponent,
+    CommonModule,
+    FormsModule,
   ],
   templateUrl: './veiculos.component.html',
   styleUrl: './veiculos.component.css',
 })
-export class VeiculosComponent {}
+export class VeiculosComponent {
+  @Output() filtrosMudaram = new EventEmitter<any>();
+
+  categoriaSelecionada: string = '';
+  veiculoNome: String = '';
+  combustivelSelecionado: string = '';
+  statusSelecionado: string = '';
+
+  aplicarFiltros() {
+    this.filtrosMudaram.emit({
+      categoria: this.categoriaSelecionada,
+      nome: this.veiculoNome,
+      combustivel: this.combustivelSelecionado,
+      status: this.statusSelecionado,
+    });
+  }
+}

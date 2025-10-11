30497ce2e1d90cb2c4fe06a050d5dfeaa9f134c8
Uelisson <tagots09@gmail.com>
2025-10-11 10:42:12 -0300

    Tentando corrigir bug do veiculo id string

--- src/app/components/detalhes/section-galeria-e-comprar/section-galeria-e-comprar.component.ts
+++ src/app/components/detalhes/section-galeria-e-comprar/section-galeria-e-comprar.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarsService } from '../../../core/services/cars.service';
-import { Observable } from 'rxjs';
+import { Observable, switchMap } from 'rxjs';
 import { Cars } from '../../../core/models/cars.model';
 import { CommonModule } from '@angular/common';
 import { GaleriaComponent } from '../galeria/galeria.component';
@@ -25,9 +25,14 @@ export class SectionGaleriaEComprarComponent implements OnInit {
   public car$!: Observable<Cars>;
 
   ngOnInit(): void {
-    const carId = this.route.snapshot.paramMap.get('id');
-    if (carId) {
-      this.car$ = this.carsService.getCar(+carId);
-    }
+    this.car$ = this.route.paramMap.pipe(
+      switchMap((params) => {
+        const carId = params.get('id');
+        if (carId) {
+          return this.carsService.getCar(+carId);
+        }
+        return new Observable<Cars>();
+      })
+    );
   }
 }
